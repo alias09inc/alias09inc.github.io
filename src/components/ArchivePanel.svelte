c51a6bcc481a2ecd1c6bf7e13bbb68a36472a372
Kent <73377814+alias09inc@users.noreply.github.com>
2026-01-25 11:22:32 +0900

    refactor: Remove `$state` from `mode` and add Astro props type in `LightDarkSwitch`, and initialize and conditionally assign `tags` and `categories` props in `ArchivePanel`.

--- src/components/ArchivePanel.svelte
+++ src/components/ArchivePanel.svelte
@@ -5,13 +5,13 @@ import I18nKey from "../i18n/i18nKey";
 import { i18n } from "../i18n/translation";
 import { getPostUrlBySlug } from "../utils/url-utils";
 
-export let tags: string[];
-export let categories: string[];
+export let tags: string[] = [];
+export let categories: string[] = [];
 export let sortedPosts: Post[] = [];
 
 const params = new URLSearchParams(window.location.search);
-tags = params.has("tag") ? params.getAll("tag") : [];
-categories = params.has("category") ? params.getAll("category") : [];
+if (params.has("tag")) tags = params.getAll("tag");
+if (params.has("category")) categories = params.getAll("category");
 const uncategorized = params.get("uncategorized");
 
 interface Post {
